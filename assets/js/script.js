5c095c257915925f3a66388db7a6c950bbcd8599
JJLindsey <jlindsey010@gmail.com>
2021-05-12 11:22:32 -0500

    add moment js to to handle date time functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,7 @@ var tasks = {};
 var createTask = function(taskText, taskDate, taskList) {
   // create elements that make up a task item
   var taskLi = $("<li>").addClass("list-group-item");
+
   var taskSpan = $("<span>")
     .addClass("badge badge-primary badge-pill")
     .text(taskDate);
@@ -13,6 +14,8 @@ var createTask = function(taskText, taskDate, taskList) {
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
 
+// check due date
+  auditTask(taskLi);
 
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
@@ -93,12 +96,20 @@ $(".list-group").on("click", "span", function() {
     .val(date);
   //swap out elements
   $(this).replaceWith(dateInput);
+  // enable jQuery UI datepicker
+  dateInput.datepicker({
+    minDate: 1,
+    onClose: function() {
+      //whne calendar is closed, force a "change" event on the 'dateInput'
+      $(this).trigger("change");
+    }
+  });
   //automatically focus on new element
   dateInput.trigger("focus");
 });
 
 // value of due date was changed
-$(".list-group").on("blur", "input[type='text']", function() {
+$(".list-group").on("change", "input[type='text']", function() {
   // get current text
   var date = $(this)
     .val()
@@ -122,6 +133,9 @@ $(".list-group").on("blur", "input[type='text']", function() {
     .text(date);
   // replace input with span element
   $(this).replaceWith(taskSpan);
+
+  //Pass task's <li> element into auditTasks() to check new due date
+  auditTask($(taskSpan).closest(".list-group-item"));
 });
 
 // modal was triggered
@@ -220,6 +234,28 @@ $(".card .list-group").sortable({
   }
 });
 
+//audit tasks
+var auditTask = function(taskEl) {
+  //get date from task  element
+  var date = $(taskEl).find("span").text().trim();
+  // to ensure it worked
+  //console.log(date);
+
+  //convert to moment object at 5:00pm
+  var time = moment(date, "L").set("hour", 17);
+  // this should print our an object for the value of the date variable, but at 5:00 pm of that dat
+  //console.log(time);
+  //remove any old classes from element
+  $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
+  // apply new class if task is near/over due date
+  if (moment().isAfter(time)) {
+    $(taskEl).addClass("list-group-item-danger");
+  } else if (Math.abs(moment().diff(time, "days")) <= 2) {
+    $(taskEl).addClass("list-group-item-warning");
+  }
+
+};
+
 $("#trash").droppable({
   accept: ".card .list-group-item",
   tolerance: "touch",
@@ -233,6 +269,11 @@ $("#trash").droppable({
   out: function(event, ui) {
     console.log("out");
   }
+});
+
+//add date picker
+$("#modalDueDate").datepicker({
+  minDate: 1
 });
 
 // load tasks for the first time
